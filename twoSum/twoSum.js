34718e36f5fcd76801f05ce586087a9419d6e7c1
vpvnguyen <vpvnguyen@gmail.com>
2019-12-17 19:27:17 -0800

    twosum hardcoded

---
+++ twoSum/twoSum.js
@@ -0,0 +1,31 @@
+// Easy
+
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var nums = [2, 7, 11, 15];
+
+var target = 9;
+
+var twoSum = function(nums, target) {
+
+    if (nums[0] + nums[1] === target) {
+        return [0, 1];
+    }
+};
+
+console.log(twoSum(nums, target));
