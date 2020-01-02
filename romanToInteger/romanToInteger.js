8812ed845a75e9fd8ab69d934e8e8c32a2f595c5
Vincent Nguyen <vpvnguyen@gmail.com>
2020-01-02 12:23:31 -0800

    add python example solution

--- romanToInteger/romanToInteger.js
+++ romanToInteger/romanToInteger.js
@@ -39,6 +39,72 @@
 // Input: "MCMXCIV"
 // Output: 1994
 // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+// =====================================================================================
+// Algorithm to convert Roman Numerals to Integer Number :
+
+// Split the Roman Numeral string into Roman Symbols (character).
+// Convert each symbol of Roman Numerals into the value it represents.
+// Take symbol one by one from starting from index 0:
+// If current value of symbol is greater than or equal to the value of next symbol, then add this value to the running total.
+// else subtract this value by adding the value of next symbol to the running total.
+// =====================================================================================
+// # Python program to convert Roman Numerals 
+// # to Numbers 
+  
+// # This function returns value of each Roman symbol 
+// def value(r): 
+//     if (r == 'I'): 
+//         return 1
+//     if (r == 'V'): 
+//         return 5
+//     if (r == 'X'): 
+//         return 10
+//     if (r == 'L'): 
+//         return 50
+//     if (r == 'C'): 
+//         return 100
+//     if (r == 'D'): 
+//         return 500
+//     if (r == 'M'): 
+//         return 1000
+//     return -1
+  
+// def romanToDecimal(str): 
+//     res = 0
+//     i = 0
+  
+//     while (i < len(str)): 
+  
+//         # Getting value of symbol s[i] 
+//         s1 = value(str[i]) 
+  
+//         if (i+1 < len(str)): 
+  
+//             # Getting value of symbol s[i+1] 
+//             s2 = value(str[i+1]) 
+  
+//             # Comparing both values 
+//             if (s1 >= s2): 
+  
+//                 # Value of current symbol is greater 
+//                 # or equal to the next symbol 
+//                 res = res + s1 
+//                 i = i + 1
+//             else: 
+  
+//                 # Value of current symbol is greater 
+//                 # or equal to the next symbol 
+//                 res = res + s2 - s1 
+//                 i = i + 2
+//         else: 
+//             res = res + s1 
+//             i = i + 1
+  
+//     return res 
+  
+// # Driver code 
+// print("Integer form of Roman Numeral is"), 
+// print(romanToDecimal("MCMIV"))  
 
 /**
  * @param {string} s
