ccd540edefc4dbc6888f8572bf2a81edef8cd953
Vincent Nguyen <vpvnguyen@gmail.com>
2021-04-15 22:52:27 -0700

    solved 2 different problems for fun with anagrams

---
+++ hacker-rank/fun-with-anagrams/README.md
@@ -0,0 +1,30 @@
+# Fun With Anagrams
+
+## Problem 1
+
+Given an array of strings, remove each string that is an anagram of an earlier string, then return the remaining array in sorted order.
+
+## Example
+
+text = ['code', 'doce', 'ecod', 'framer', 'frame']
+
+- 'code' and 'doce' are anagrams. Remove 'doce' from the array and keep the first occurrence code in the array.
+- 'code' and 'ecod' are anagrams. Remove 'ecod' from the array and keep the first occurrence code in the array.
+- 'code' and 'framer' are not anagrams. Keep both strings in the array.
+- 'framer' and 'frame' are not anagrams due to the extra 'r' in 'framer'. Keep both strings in the array.
+- Order the remaining strings in ascending order: ['code','frame','framer'].
+
+## Problem 2
+
+Given an array of strings, determine if any of them are anagrams then remove any of the subsequent anagrams.
+Return an array of the first instance of each anagram that is sorted in ascending order.
+
+## Example
+
+text = ["code", "doce", "ecod", "framer", "frame", "remarf"]
+
+- 'code' and 'doce' are anagrams. Remove 'doce' from the array and keep the first occurrence code in the array.
+- 'code' and 'ecod' are anagrams. Remove 'ecod' from the array and keep the first occurrence code in the array.
+- 'framer' and 'frame' are not anagrams due to the extra 'r' in 'framer'. Remove 'frame' from the array.
+- 'framer' and 'remarf' are anagrams. Remove 'remarf' from the array and keep the first occurrence code in the array.
+- Order the remaining strings in ascending order: ['code','framer'].
--- hacker-rank/fun-with-anagrams/solution.js
+++ hacker-rank/fun-with-anagrams/solution.js
@@ -1,34 +1,101 @@
-function funWithAnagrams(text) {
-  // Create a map to store occurrences of strings
-  // If there are more than 1 occurrences, there has been a match and it is an anagram
+// funWithAnagrams - Problem 1 ====================================
+// Given an array of strings,
+// remove each string that is an anagram of an earlier string,
+// then return the remaining array in ascending order.
+
+// Sample Input
+// ["code", "doce", "ecod", "framer", "frame", "remarf"]
+// Sample Output
+// ['code', 'frame', 'framer']
+const funWithAnagrams = (text) => {
+  // Create a map to store the occurrences of strings
   const map = {};
 
-  // Create a function to sort strings alphabetically
+  // Create a function to sort and return the string in alphabetical order
+  const sortString = (string) => string.split("").sort().join("");
+
+  // Iterate through each string from the given array
+  // and return a new array of alphabetically sorted strings
+  const sortedStringsInGivenArray = text.map((value) => sortString(value));
+
+  // Iterate through the array of sorted strings
+  // If the sorted string does not exist in the map object,
+  // set the sorted string as it's property and set it's value as the original string
+  sortedStringsInGivenArray.forEach((item, index) => {
+    if (!map[item]) map[item] = text[index];
+  });
+
+  // Create an array of the original strings from the values of the map object
+  const mapValues = Object.values(map);
+
+  // Sort the array of original strings in ascending order
+  const sortedAnswer = mapValues.sort();
+
+  return sortedAnswer;
+};
+
+// funWithAnagrams2 - Problem 2 ====================================
+// Given an array of strings,
+// determine if any of them are anagrams
+// then remove any of the subsequent anagrams.
+// Return an array of the first instance of each anagram that is sorted in ascending order.
+
+// Sample Input
+// ["code", "doce", "ecod", "framer", "frame", "remarf"]
+// Sample Output
+// ['code', 'framer']
+
+const funWithAnagrams2 = (text) => {
+  // Create a map to store the occurrences of strings
+  const map = {};
+
+  // Create an array to store the answers
+  const answers = [];
+
+  // Iterate through the strings from the given array
+  // and return a new array of alphabetically sorted strings
   const sortString = (string) => {
     return string.split("").sort().join("");
   };
 
-  // Sort the strings in the array and return a new array
-  const sortedStringsInArray = text.map((value) => {
-    return sortString(value);
+  // Iterate through each string from the given array
+  // For each string, sort the string alphabetically
+  // If the sorted string exists as a key on the map, increment it's count property by 1
+  // If the sorted string does not exist as a key on the map,
+  // set the original string as the firstElement
+  // and set it's count to 1
+  // We do it in this specific order, otherwise, it will falsely increment
+  text.forEach((item) => {
+    const sortedString = sortString(item);
+
+    if (map[sortedString]) {
+      map[sortedString].count += 1;
+    }
+
+    if (!map[sortedString]) {
+      map[sortedString] = {
+        firstElement: item,
+        count: 1,
+      };
+    }
   });
 
-  // Iterate through the sorted strings within the array
-  // If the string exists as a key within the map object, increment the occurrence by 1
-  // If the string does not exist as a key within the map object, set it as a key and set the occurrence to 1
-  sortedStringsInArray.forEach((item) => {
-    if (map[item]) map[item] += 1;
-    if (!map[item]) map[item] = 1;
+  // Create an array of the string's firstElement and count
+  // Ex.: [{ firstElement: 'code', count: 1 }]
+  const mapValues = Object.values(map);
+
+  // Iterate through the array of the string's firstElement and count
+  // If the string's count is greater than 1, push the string's firstElement to the answers array
+  mapValues.forEach(({ firstElement, count }) => {
+    if (count > 1) answers.push(firstElement);
   });
 
-  // Iterate through the keys of the map object and return a new array of strings that have occurrences higher than 1
-  const answer = Object.keys(map).filter((value) => {
-    if (map[value] > 1) return value;
-  });
+  return answers;
+};
 
-  return console.log(answer);
-}
+// Input
+const text = ["code", "doce", "ecod", "framer", "frame", "remarf"];
 
-const text = ["code", "aaagmnrs", "anagrams", "doce", "doce", "something else"];
-
-funWithAnagrams(text);
+// Output
+funWithAnagrams(text); // [ 'code', 'frame', 'framer' ]
+funWithAnagrams2(text); // [ 'code', 'framer' ]
